16a7a2fb2ef4505482e3089052c03e381622b161
Kyle <kyleyu23@gmail.com>
2021-10-14 17:48:57 -0500

    add UI

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
 function computerPlay() {
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
     computer = getRandomInt(3);
     switch (computer) {
         case 0:
@@ -14,171 +15,46 @@ function computerPlay() {
         default:
             return "something went wrong";
     }
+
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    playerSelection = this.id;
+    computerSelection = computerPlay();
 
-    //toUpperCase
+    // //toUpperCase
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
 
-    //tie
-    if (computerSelection === playerSelection) {
-        return 0;
-    }
     //computer wins
     if ((computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
         (computerSelection === "PAPER" && playerSelection === "ROCK") ||
         (computerSelection === "SCISSORS" && playerSelection === "PAPER")
     ) {
-        return 1;
+        computerScore.textContent++;
     }
     //player wins 
     if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
         (playerSelection === "PAPER" && computerSelection === "ROCK") ||
         (playerSelection === "SCISSORS" && computerSelection === "PAPER")
     ) {
-        return 2;
+        playerScore.textContent++;
+    }
+    //winner
+    if (playerScore.textContent == scoreToWin) {
+        result.textContent = "You Won!";
+    }
+    if (computerScore.textContent == scoreToWin) {
+        result.textContent = "You Lost!";
     }
 }
 
-function game() {
-    // playerScore = 0, computerScore = 0;
-    let scores = {
-        player: 0,
-        computer: 0,
-    };
 
-    for (let rounds = 0; rounds < 5; rounds++) {
+const playerButtons = document.querySelectorAll('.playerSelection');
+playerButtons.forEach(button => button.addEventListener('click', playRound));
 
-        playerSelection = getPlayerSelection();
+const result = document.querySelector('.result');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
 
-        roundResult = playRound(playerSelection, computerPlay());
-
-        scores = calcScore(roundResult, scores);
-
-        displayRoundResult(roundResult);
-
-    }
-    return;
-}
-
-function getPlayerSelection() {
-    return prompt("rock, paper, or scissors?");
-    // return "rock";
-}
-
-/** 
- * @param {Number!} result
- * @param {{player: Number!, computer: Number!}!} scores State containing the scores
- * @return {{player: Number!, computer: Number!}!}
- */
-function calcScore(result, scores) {
-    let computerScore = scores.computer;
-    let playerScore = scores.player;
-    switch (result) {
-        case 0:
-            break;
-        case 1:
-            computerScore++;
-            break;
-        case 2:
-            playerScore++;
-            break;
-        default:
-            throw new Error("something went wrong");
-    }
-    return { player: playerScore, computer: computerScore };
-}
-
-function getRoundMessage(result) {
-
-    switch (result) {
-        case 0:
-            return "Tie";
-        case 1:
-            return "Computer Wins";
-        case 2:
-            return "Player Wins";
-        default:
-            return "something went wrong";
-    }
-}
-
-function displayRoundResult(result) {
-    console.log(getRoundMessage(result));
-}
-
-function displayScore(playerScore, computerScore) {
-    console.log(`Player: "${playerScore}" Computer: "${computerScore}"`);
-}
-
-// let playerScore, computerScore;
-
-
-// Rock -> 0
-// Paper -> 1
-// Scissors -> 2
-
-// a, b
-// 0, 0 -> tie
-// 0, 1 -> b wins
-// 0, 2 -> a wins
-// 1, 0 -> 
-// 1, 1 -> tie
-// 1, 2 -> 
-// 2, 0 -> 
-// 2, 1 -> 
-// 2, 2 -> tie
-
-// // a equals b?
-// a === b --> ties
-// // b is the sucessor of a
-// (a + 1) % 3 === b --> b wins
-// // a is the sucessor of b
-// (b + 1) % 3 === a --> a wins
-
-
-// class Game {
-//     computerScore = 0;
-//     playerScore = 0;
-
-//     play() {
-//         for (let rounds = 0; rounds < 5; rounds++) {
-//             playerSelection = this.getPlayerSelection();
-//             roundResult = playRound(playerSelection, computerPlay());
-//             scores = calcScore(roundResult, scores);
-//             displayRoundResult(roundResult);
-//         }
-//     }
-
-//     getPlayerSelection() {
-//         return "rock";
-//     }
-
-//     playRound(playerSelection, computerSelection) {
-
-//     }
-
-//     /** 
-//      * @param {Number!} result
-//      * @return {void}
-//      */
-//     updateScore(result) {
-//         switch (result) {
-//             case 0:
-//                 break;
-//             case 1:
-//                 this.computerScore++;
-//                 break;
-//             case 2:
-//                 this.playerScore++;
-//                 break;
-//             default:
-//                 throw new Error("something went wrong");
-//         }
-//     }
-// }
-
-// const game = new Game();
-// game.play();
+const scoreToWin = 5;
